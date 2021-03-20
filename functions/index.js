a8df4cedba053f14c8d700ef2d3de911eff51014
bahrozjaved <bahrozjaved6@gmail.com>
2021-03-21 02:40:43 +0500

    project added

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase);
+const database = admin.database();
+
+exports.helloiot = functions.https.onRequest(async(request, response) => {
+
+    let params = request.body.queryResult.parameters;
+console.log("params are",params)
+
+await database.ref().update(params);
+  response.send("Hello from Google Assistant iot bot");
+});
